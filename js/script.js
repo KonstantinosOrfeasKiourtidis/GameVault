e4c5833c5213aef91e1031ddc71215fdb848b2df
Konstantinos Orfeas Kiourtidis <60224852+KonstantinosOrfeasKiourtidis@users.noreply.github.com>
2022-03-17 14:55:28 +0200

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+
+    const href = link.getAttribute("href");
+
+    if (href === "") {
+      return
+    }
+
+    // Scroll back to top
+    if (href === "#") {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      e.preventDefault();
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link")) {
+      e.preventDefault();
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const mediaQuery = window.matchMedia('(max-width: 940px)')
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+
+    if (ent.isIntersecting === false) {
+
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+
+
